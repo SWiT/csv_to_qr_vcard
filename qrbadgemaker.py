36c1a9b91429e10a52092a5b081843c2fa9bcd3e
Matt Switlik <switlik@oakland.edu>
2015-06-03 16:07:44 -0400

    added --backcredits option

--- qrbadgemaker.py
+++ qrbadgemaker.py
@@ -11,17 +11,26 @@ def displayHelp():
     print "\tpython "+sys.argv[0]+" [OPTION] \"EVENTNAME\" \"HASHTAG\" CSVFILE"
     print "Example:"
     print "\tpython "+sys.argv[0]+" \"Conference 2015\" \"#Con2015\" attendees.csv"
-    print "OPTION:"
+    print "OPTIONS:"
     print "\t--drawtemplate\tdraw the B-475 template lines"
+    print "\t--backcredits\tdraw a back page with credit & URL to QR Badge Maker."
+    print "\t\t\tNo other parameters are required."
     print ""
 
    
-if len(sys.argv) < 4 or len(sys.argv) > 5:
+if len(sys.argv) < 2 or len(sys.argv) > 5:
     displayHelp()
     quit()
 
 drawtemplate = False
-if sys.argv[1] == "--drawtemplate":
+backcredits  = False
+
+if sys.argv[1] == "--backcredits":
+    backcredits = True
+    eventname   = ""
+    hashtag     = ""
+    csvfile     = "back_credits.csv"
+elif sys.argv[1] == "--drawtemplate":
     drawtemplate = True
     if len(sys.argv) == 5:
         eventname   = sys.argv[2]
@@ -30,18 +39,33 @@ if sys.argv[1] == "--drawtemplate":
     else:
         displayHelp()
         quit()
-else:
+elif len(sys.argv) == 4:
     eventname   = sys.argv[1]
     hashtag     = sys.argv[2]
     csvfile     = sys.argv[3]
+else:
+    displayHelp()
+    quit()
 
 filename    = os.path.splitext(csvfile)[0]
-directory   = filename
 
-pdf = canvas.Canvas(filename+".pdf", pagesize=letter)
+if backcredits:
+    pdf = canvas.Canvas("back_credits.pdf", pagesize=letter)
+else:
+    pdf = canvas.Canvas(filename+".pdf", pagesize=letter)
 
 
-def drawBadge(pos):
+def drawBadge(pos, backcredits=False):
+    if backcredits:
+        # Draw the "made using blurb and url"
+        pdf.setFont("Helvetica", 11)
+        x = pos[0]
+        y = pos[1] - 0.75*inch
+        pdf.drawCentredString(x,y, "This badge and QR code was made using")
+        y = pos[1] - 0.9*inch
+        pdf.drawCentredString(x,y, "https://github.com/swit/qrbadgemaker")
+        return
+    
     # Draw the event title
     pdf.setFont("Helvetica-Bold", 27)
     x = pos[0]
@@ -63,7 +87,7 @@ def drawBadge(pos):
     # Draw the QR code
     x = pos[0] - 1.0*inch
     y = pos[1]- 3.9*inch
-    pdf.drawImage(imgfile, x,y, width=2.0*inch, height=2.0*inch)
+    pdf.drawInlineImage(imgfile, x,y, width=2.0*inch, height=2.0*inch)
     
     # Draw the hashtag
     pdf.setFont("Helvetica-Bold", 17)
@@ -71,86 +95,82 @@ def drawBadge(pos):
     y = pos[1] - 5.5*inch
     pdf.drawCentredString(x,y, hashtag)
     
-    # Draw the "made using blurb and url"
-    #pdf.setFont("Helvetica", 11)
-    #x = pos[0]
-    #y = pos[1] - 5.35*inch
-    #pdf.drawCentredString(x,y, "This badge and QR code was made using")
-    #y = pos[1] - 5.5*inch
-    #pdf.drawCentredString(x,y, "https://github.com/swit/qrbadgemaker")
-    
-if not os.path.exists(csvfile):
-    print "Error: File '"+csvfile+"' not found."
+if backcredits:
+    cvsfile = [[".",".",".","."],[".",".",".","."],[".",".",".","."],[".",".",".","."]]
+elif not os.path.exists(csvfile):
+    print
+    print "ERROR: File '"+csvfile+"' not found."
+    print
     displayHelp()
     quit()
-    
-with open(csvfile, 'rb') as csvfp:
-    
-    if not os.path.exists(directory):
-        os.makedirs(directory)
-    
-    pagepos = 0
-    badgecount = 0
+else:
+    csvfp = open(csvfile, 'rb')
     cvsfile = csv.reader(csvfp)
-    for row in cvsfile:
-    	if len(row) <= 0:
-    	    continue
-    	if row[0][0] == '#':
-    	    continue
-    	firstname = row[0].strip()
-        lastname = row[1].strip()
-        fullname = firstname+" "+lastname
-        institution = row[2].strip()
-        email = row[3].strip()
 
-        if fullname != "":
-            qrcontent = "BEGIN:VCARD\n"
-            qrcontent += "VERSION:3.0\n"
-            qrcontent += "N:"+fullname+"\n"
-            if institution != "":
-                qrcontent += "ORG:"+institution+"\n"
-            if email != "":
-                qrcontent += "EMAIL:"+email+"\n"
-            qrcontent += "END:VCARD\n"
+pagepos = 0
+badgecount = 0
 
-            qr = qrcode.QRCode(error_correction=qrcode.ERROR_CORRECT_L)
-            qr.add_data(qrcontent)
-            qr.make()
-            im = qr.make_image()
-            imgfile = directory+"/"+lastname+"_"+firstname+".png"
-            im.save(imgfile)
+for row in cvsfile:
+    if len(row) <= 0:
+        continue
+        
+    if row[0][0] == '#':
+        continue
+
+    firstname = row[0].strip()
+    lastname = row[1].strip()
+    fullname = firstname+" "+lastname
+    institution = row[2].strip()
+    email = row[3].strip()
+
+    if fullname != "":
+        qrcontent = "BEGIN:VCARD\n"
+        qrcontent += "VERSION:3.0\n"
+        qrcontent += "N:"+fullname+"\n"
+        if institution != "":
+            qrcontent += "ORG:"+institution+"\n"
+        if email != "":
+            qrcontent += "EMAIL:"+email+"\n"
+        qrcontent += "END:VCARD\n"
+
+        qr = qrcode.QRCode(error_correction=qrcode.ERROR_CORRECT_L)
+        qr.add_data(qrcontent)
+        qr.make()
+        im = qr.make_image()
+        imgfile = "temp.png"
+        im.save(imgfile)
+        
+        if drawtemplate:
+            # B-475 Template lines
+            pdf.line(0,0.125*inch, 8.5*inch,0.125*inch)
+            pdf.line(0,5.4375*inch, 8.5*inch,5.4375*inch)
+            pdf.line(0,10.625*inch, 8.5*inch,10.625*inch)
+            pdf.line(4.25*inch,0.125*inch, 4.25*inch,10.625*inch)
+        
+        # There seems to be a bug in reportlab.
+        # Strings are draw in the A4 template even though we've set the pagesize to letter.
+        # Do text positioning based on A4.
+        if pagepos == 0:
+            pos = A4[0]/4, A4[1]-0.625*inch
+        elif pagepos == 1:
+            pos = A4[0]*3/4+0.25*inch, A4[1]-0.625*inch
+        elif pagepos == 2:
+            pos = A4[0]/4, A4[1]/2
+        elif pagepos == 3:
+            pos = A4[0]*3/4+0.25*inch, A4[1]/2
+        drawBadge(pos, backcredits)
             
-            if drawtemplate:
-                # B-475 Template lines
-                pdf.line(0,0.125*inch, 8.5*inch,0.125*inch)
-                pdf.line(0,5.4375*inch, 8.5*inch,5.4375*inch)
-                pdf.line(0,10.625*inch, 8.5*inch,10.625*inch)
-                pdf.line(4.25*inch,0.125*inch, 4.25*inch,10.625*inch)
-            
-            # There seems to be a bug in reportlab.
-            # Strings are draw in the A4 template even though we've set the pagesize to letter.
-            # Do text positioning based on A4.
-            if pagepos == 0:
-                pos = A4[0]/4, A4[1]-0.625*inch
-            elif pagepos == 1:
-                pos = A4[0]*3/4+0.25*inch, A4[1]-0.625*inch
-            elif pagepos == 2:
-                pos = A4[0]/4, A4[1]/2
-            elif pagepos == 3:
-                pos = A4[0]*3/4+0.25*inch, A4[1]/2
-            drawBadge(pos)
-                
-            if pagepos >= 3:
-                pdf.save()
-                pagepos = -1
-            
-            pagepos += 1
-            badgecount += 1
-            
-    if pagepos != 0:
-        pdf.save()
-    
-    print        
-    print str(badgecount)+" Badges created in "+filename+".pdf"
-    print
+        if pagepos >= 3:
+            pdf.save()
+            pagepos = -1
+        
+        pagepos += 1
+        badgecount += 1
+        
+if pagepos != 0:
+    pdf.save()
+
+print        
+print str(badgecount)+" Badges created in "+filename+".pdf"
+print
     
